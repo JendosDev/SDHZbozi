913d986449326d071572dd90dbcf0448a91d5628
vanya <mitrofanovvana20082@gmail.com>
2026-05-20 09:20:00 +0200

    format: add maven dependencies

--- src/main/java/com/example/sdhzbozi/SdhZboziApplication.java
+++ src/main/java/com/example/sdhzbozi/SdhZboziApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SdhZboziApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SdhZboziApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SdhZboziApplication.class, args);
+    }
 
 }
