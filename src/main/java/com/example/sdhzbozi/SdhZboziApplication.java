049fc8830826da2124b4c03e0e0333a691c98586
vanya <mitrofanovvana20082@gmail.com>
2026-05-20 09:13:44 +0200

    Initial commit

---
+++ src/main/java/com/example/sdhzbozi/SdhZboziApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdhzbozi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdhZboziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdhZboziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sdhzbozi/SdhZboziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdhzbozi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdhZboziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
